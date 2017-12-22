4e131e0fdf02e7c048b09520244a8d76feb33dd7
Bell App Lab <apps@bellapplab.com>
2017-12-22 11:50:29 +0100

    Done

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Bell App Lab on 22.12.17.
+//  Copyright © 2017 Bell App Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
+
